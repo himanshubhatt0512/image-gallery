0737799a77a817a1ef6a3221be9cbcfae89e171c
himanshubhatt0512 <iamhimanshubhatt@gmail.com>
2023-01-23 02:17:45 +0530

    Title complete

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Galleria</h1>
+      <h2>Welcome to your own gallery</h2>
+      <p>Here are Some of your memories, You can upload your photos by clicking this + button</p>
     </div>
   )
 }
