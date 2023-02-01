52af41f723b0841796a11021363af664cb09e3b2
himanshubhatt0512 <iamhimanshubhatt@gmail.com>
2023-02-01 17:48:49 +0530

    almost complete

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>Galleria</h1>
+      <h1>Image_Gallery </h1>
       <h2>Welcome to your own gallery</h2>
       <p>Here are Some of your memories, You can upload your photos by clicking this + button</p>
     </div>
